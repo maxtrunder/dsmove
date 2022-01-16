5fbcef8edea761c8f0805a76c9c3c214267c5634
Mackcoel <maxtrunder@hotmail.com>
2022-01-16 05:41:02 -0300

    Save score

---
+++ backend/src/main/java/com/devsuperior/dsmove/controllers/ScoreController.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsmove.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmove.dto.MovieDTO;
+import com.devsuperior.dsmove.dto.ScoreDTO;
+import com.devsuperior.dsmove.services.ScoreService;
+
+@RestController
+@RequestMapping(value ="/scores") 
+public class ScoreController {
+	
+	
+@Autowired
+ private ScoreService service;
+    
+    @PutMapping
+  	public MovieDTO saveScore( @RequestBody ScoreDTO dto){
+  		MovieDTO movieDTO = service.saveScore(dto);
+  		return movieDTO;
+  	}
+}
